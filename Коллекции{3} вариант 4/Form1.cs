ed7fa2ccaaf228d37539f783814628bb9296919f
werdi51 <spotifig2@gmail.com>
2025-04-12 17:08:10 +0500

    Add files via upload

---
+++ Коллекции{3} вариант 4/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Коллекции_3__ArrayLisdt
+{
+    public partial class Form1 : Form
+    {
+        int counter = 0;
+        int CollectionLength = 0;
+        ArrayList collection = new ArrayList();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            CollectionLength = (int)numericUpDown1.Value;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(CollectionLength.ToString());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (CollectionLength <= collection.Count)
+            {
+                MessageBox.Show("Достигнута граница массива");
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(textBox2.Text))
+            {
+                MessageBox.Show("Введите текст!");
+                return;
+            }
+
+            collection.Add(textBox2.Text);
+            counter++;
+
+            listBox1.Items.Clear();
+            foreach (var item in collection)
+            {
+                listBox1.Items.Add(item);
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex == -1)
+            {
+                MessageBox.Show("Выберите элемент из списка!");
+                return;
+            }
+
+            object selectedItem = collection[listBox1.SelectedIndex];
+
+            bool isNumber = double.TryParse(selectedItem.ToString(), out double selectedNumber);
+            if (!isNumber)
+            {
+                MessageBox.Show("Выбранное значение не является числом!");
+                return;
+            }
+
+            if (!double.TryParse(textBox1.Text, out double inputNumber))
+            {
+                MessageBox.Show("Вы ввели не число!");
+                return;
+            }
+
+            MessageBox.Show($"Ответ: {selectedNumber * inputNumber}");
+        }
+    }
+}
---
+++ коллекции 3 вариант3/Vector.cs
@@ -0,0 +1,63 @@
+﻿class Vector
+{
+    public double X1 { get; set; }
+    public double Y1 { get; set; }
+    public double Z1 { get; set; }
+    public double X2 { get; set; }
+    public double Y2 { get; set; }
+    public double Z2 { get; set; }
+
+    public Vector(double x1, double y1, double z1, double x2, double y2, double z2)
+    {
+        X1 = x1;
+        Y1 = y1;
+        Z1 = z1;
+        X2 = x2;
+        Y2 = y2;
+        Z2 = z2;
+    }
+
+    public double[] GetCoordinates()
+    {
+        return new double[] { X2 - X1, Y2 - Y1, Z2 - Z1 };
+    }
+
+    public static Vector operator +(Vector v1, Vector v2)
+    {
+        double[] coords1 = v1.GetCoordinates();
+        double[] coords2 = v2.GetCoordinates();
+        return new Vector(0, 0, 0, coords1[0] + coords2[0], coords1[1] + coords2[1], coords1[2] + coords2[2]);
+    }
+
+    public static Vector operator -(Vector v1, Vector v2)
+        //вычит
+    {
+        double[] coords1 = v1.GetCoordinates();
+        double[] coords2 = v2.GetCoordinates();
+        return new Vector(0, 0, 0, coords1[0] - coords2[0], coords1[1] - coords2[1], coords1[2] - coords2[2]);
+    }
+
+    public static double DotProduct(Vector v1, Vector v2)
+    {
+        double[] coords1 = v1.GetCoordinates();
+        double[] coords2 = v2.GetCoordinates();
+        return coords1[0] * coords2[0] + coords1[1] * coords2[1] + coords1[2] * coords2[2];
+    }
+
+    public double Length()
+    {
+        double[] coords = GetCoordinates();
+        return Math.Sqrt(coords[0] * coords[0] + coords[1] * coords[1] + coords[2] * coords[2]);
+    }
+
+    public static double CosAngle(Vector v1, Vector v2)
+    {
+        return DotProduct(v1, v2) / (v1.Length() * v2.Length());
+    }
+
+    public override string ToString()
+    {
+        double[] coords = GetCoordinates();
+        return $"({coords[0]}, {coords[1]}, {coords[2]})";
+    }
+}
